1adf29a1421c6716c3b91b58615436e6a1ba119e
nyeong-deo <131740017+nyeong-deo@users.noreply.github.com>
2024-08-22 21:55:32 +0900

    Feat: 첫 카카오 로그인일 때 db에 user정보를 저장해 회원가입

--- services/kakaoLoginService.js
+++ services/kakaoLoginService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const qs = require('qs');
 const { authURL, tokenURL, userInfoURL, clientId, redirectUri } = require('../config/kakaoLogin');
-const User = require('../models/userModel');
+const User = require('../models/memberModel');
 
 const getAuthURL = () => `${authURL}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
 
