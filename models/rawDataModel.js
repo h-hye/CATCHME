5ff21d4b0ce2e16e5c622516b8ccc754fbeb5d45
nyeong-deo <131740017+nyeong-deo@users.noreply.github.com>
2024-09-10 15:22:09 +0900

    Refactor: 연관관계 매핑

---
+++ models/rawDataModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const rawDataSchema = new mongoose.Schema(
+    {
+        userId:{
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User', // User 모델과 연결
+            required: true 
+        },
+        sensors:{
+            type:[Number],
+            required:true        
+        }
+        
+    },
+    {
+        timestamps:true
+    }
+);
+
+//스키마 -> 모델
+//mongoose.model(모델명, 스키마명)
+const RawData = mongoose.model("RawData",rawDataSchema);
+module.exports = RawData;
